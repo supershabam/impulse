6cbe8952d0310190a5fc1cd87720dfebfbae3721
Ian Hansen <ian@supershabam.com>
2014-01-29 23:54:16 -0700

    remove static, move www

--- static/source/javascripts/profile-pic.js
+++
@@ -1,9 +0,0 @@
-/** @jsx React.DOM */
-
-var ProfilePic = React.createClass({
-  render: function() {
-    return(
-      <img src={'http://graph.facebook.com/' + this.props.username + '/picture'}>
-    )
-  }
-})
